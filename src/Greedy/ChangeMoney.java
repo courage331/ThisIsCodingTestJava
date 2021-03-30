fc1e0ed064da6a13c1f866dfa181fb29174614d2
courage331 <courage331@gmail.com>
2021-03-30 21:26:35 +0900

    3-1 거스름돈

--- src/Greedy/ChangeMoney.java
+++ src/Greedy/ChangeMoney.java
@@ -1,8 +1,21 @@
 package Greedy;
 
+/** 3장 그리디 알고리즘
+ *  예제 3.1 거스름돈
+ *
+ * */
 public class ChangeMoney {
 
     public static void main(String [] args){
-        System.out.println("test");
+
+        int n = 1260; //입력값
+        int answer = 0; // 결과값
+        int [] coin = {500,100,50,10}; //동전종류
+
+        for(int i=0; i<coin.length; i++){
+            answer+=n/coin[i];
+            n = n%coin[i];
+        }
+        System.out.println("결과값 : "+answer);
     }
 }
